6a5030375c62d884cdfaab559c52328988169962
Mittapalli Pavan <118098180+Pavan-0004@users.noreply.github.com>
2023-12-16 11:41:22 +0530

    himan.py

--- himan.py
+++ himan.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+st.header("Himanshu's personal website")
+st.subheader("PPts and Pdf's")
 with open("Solid-Waste.pdf", "rb") as file:
     btn = st.download_button(
     label="Download Solid Waste pdf",
@@ -7,3 +9,4 @@ with open("Solid-Waste.pdf", "rb") as file:
     file_name="Solid-Waste.pdf",
     mime="pdf"
     )
+st.write("All PPTs [link](https://bit.ly/aiml-3-1)")
