b0aaa2c86f673a9c26be11a3b148a4fa4bdf0e7b
Mittapalli Pavan <118098180+Pavan-0004@users.noreply.github.com>
2024-01-02 23:26:18 +0530

    himan.py

--- himan.py
+++ himan.py
@@ -9,6 +9,13 @@ with open("Solid-Waste.pdf", "rb") as file:
     file_name="Solid-Waste.pdf",
     mime="pdf"
     )
+with open("Accident locations on Indian roads.ppt", "rb") as ppt:
+    btn = st.download_button(
+    label="Download PPT",
+    data=ppt,
+    file_name="Accident locations on Indian roads.ppt",
+    mime="ppt"
+            )
 st.write()
 st.write("All PPTs [link](https://bit.ly/aiml-3-1)")
 st.write("Leetcode  [profile](https://leetcode.com/Himanshu_parida)")
